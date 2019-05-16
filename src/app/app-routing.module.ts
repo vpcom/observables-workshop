8010f34cd7be4cfd5faf0df66a31ada316366d29
Vincent Perrin <vpcom01@gmail.com>
2019-05-16 22:40:37 +0200

    welcome description text

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
 import { LoadingShowcaseComponent } from './loading-showcase/loading-showcase.component';
 
 const routes: Routes = [
+  { path: '', component: WelcomeComponent },
   { path: 'loading-showcase', component: LoadingShowcaseComponent },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 import { LoadingShowcaseComponent } from './loading-showcase/loading-showcase.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    WelcomeComponent,
     LoadingShowcaseComponent
   ],
   imports: [
